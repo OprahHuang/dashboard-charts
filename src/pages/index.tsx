ec0d5a17770c68b1d2d51eb0b1c0dcb20e239780
Oprah <ohuang@omnisolu.com>
2023-12-22 23:05:09 -0800

    updated the design of totals and filters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const newChartData = [
   {
-    name: "Team A",
+    name: "Check-in",
     data: [
       { x: "Jan 2023", y: 500 },
       { x: "Feb 2023", y: 700 },
@@ -21,7 +21,7 @@ const newChartData = [
     ],
   },
   {
-    name: "Team B",
+    name: "App User",
     data: [
       { x: "Jan 2023", y: 1300 },
       { x: "Feb 2023", y: 1900 },
@@ -40,7 +40,7 @@ const newChartData = [
     ],
   },
   {
-    name: "Team C",
+    name: "New Added WiFi",
     data: [
       { x: "Jan 2023", y: 2000 },
       { x: "Feb 2023", y: 2500 },
@@ -59,7 +59,7 @@ const newChartData = [
     ],
   },
   {
-    name: "Team D",
+    name: "Issued mPoints",
     data: [
       { x: "Jan 2023", y: 3800 },
       { x: "Feb 2023", y: 3000 },
@@ -83,27 +83,115 @@ const Home: React.FC = () => {
   const [chartOptions, setChartOptions] = useState<any | null>(null);
   const [chartSeries, setChartSeries] = useState(newChartData);
 
+  const calculateSum = () => {
+    return chartSeries.map((series) =>
+      series.data.reduce(
+        (seriesTotal, dataPoint) => seriesTotal + dataPoint.y,
+        0
+      )
+    );
+  };
+
+  const calculateSumText = () => {
+    const sums = calculateSum();
+    return sums
+      .map((sum, index) => `${chartSeries[index].name} Total: ${sum}`)
+      .join(" , ");
+  };
+
+  const showSeriesSum = () => {
+    const sums = calculateSum();
+    return sums.map((sum, index) =>
+      <div key={index} className="bg-[#ffffff10] opacity-90 px-4 md:px-7 py-2 md:py-3 rounded-lg border border-[#ffffff15]">
+        <p className="text-xl text-[#e3e3e3] font-semibold tracking-wide"><span className="text-xs opacity-55 block font-light">{chartSeries[index].name}</span><span className="text-xs align-text-top font-light mr-2">Total</span>
+        <span className="text-xs align-text-top">$</span>{sum}</p>
+
+      </div>
+    )
+  }
+
 
   useEffect(() => {
     import("react-apexcharts").then(({ default: ReactApexChart }) => {
-      const calculateSum = () => {
-        return chartSeries.map((series) =>
-          series.data.reduce(
-            (seriesTotal, dataPoint) => seriesTotal + dataPoint.y,
-            0
-          )
-        );
-      };
-
-      const calculateSumText = () => {
-        const sums = calculateSum();
-        return sums
-          .map((sum, index) => `${chartSeries[index].name} Total: ${sum}`)
-          .join(" , ");
-      };
-
       const newChartOptions = {
-        series: chartSeries,
+        series: [
+          {
+            name: "Team A",
+            data: [
+              { x: "Jan 2023", y: 500 },
+              { x: "Feb 2023", y: 700 },
+              { x: "Mar 2023", y: 900 },
+              { x: "Apr 2023", y: 400 },
+              { x: "May 2023", y: 200 },
+              { x: "Jun 2023", y: 800 },
+              { x: "Jul 2023", y: 1100 },
+              { x: "Aug 2023", y: 1800 },
+              { x: "Sep 2023", y: 800 },
+              { x: "Oct 2023", y: 400 },
+              { x: "Nov 2023", y: 200 },
+              { x: "Dec 2023", y: 400 },
+              { x: "Jan 2024", y: 1000 },
+              { x: "Feb 2024", y: 800 },
+            ],
+          },
+          {
+            name: "Team B",
+            data: [
+              { x: "Jan 2023", y: 1300 },
+              { x: "Feb 2023", y: 1900 },
+              { x: "Mar 2023", y: 1300 },
+              { x: "Apr 2023", y: 1700 },
+              { x: "May 2023", y: 1100 },
+              { x: "Jun 2023", y: 1600 },
+              { x: "Jul 2023", y: 1500 },
+              { x: "Aug 2023", y: 1400 },
+              { x: "Sep 2023", y: 1900 },
+              { x: "Oct 2023", y: 1500 },
+              { x: "Nov 2023", y: 2000 },
+              { x: "Dec 2023", y: 1500 },
+              { x: "Jan 2024", y: 1900 },
+              { x: "Feb 2024", y: 1200 },
+            ],
+          },
+          {
+            name: "Team C",
+            data: [
+              { x: "Jan 2023", y: 2000 },
+              { x: "Feb 2023", y: 2500 },
+              { x: "Mar 2023", y: 2000 },
+              { x: "Apr 2023", y: 2900 },
+              { x: "May 2023", y: 2000 },
+              { x: "Jun 2023", y: 2800 },
+              { x: "Jul 2023", y: 2700 },
+              { x: "Aug 2023", y: 2800 },
+              { x: "Sep 2023", y: 2000 },
+              { x: "Oct 2023", y: 2500 },
+              { x: "Nov 2023", y: 2500 },
+              { x: "Dec 2023", y: 2700 },
+              { x: "Jan 2024", y: 2000 },
+              { x: "Feb 2024", y: 2300 },
+            ],
+          },
+          {
+            name: "Team D",
+            data: [
+              { x: "Jan 2023", y: 3800 },
+              { x: "Feb 2023", y: 3000 },
+              { x: "Mar 2023", y: 3200 },
+              { x: "Apr 2023", y: 3000 },
+              { x: "May 2023", y: 3800 },
+              { x: "Jun 2023", y: 3000 },
+              { x: "Jul 2023", y: 3400 },
+              { x: "Aug 2023", y: 3000 },
+              { x: "Sep 2023", y: 3400 },
+              { x: "Oct 2023", y: 3200 },
+              { x: "Nov 2023", y: 3400 },
+              { x: "Dec 2023", y: 3700 },
+              { x: "Jan 2024", y: 3500 },
+              { x: "Feb 2024", y: 3200 },
+            ],
+          },
+        ],
         chart: {
           height: 600,
           type: "rangeArea",
@@ -179,7 +267,7 @@ const Home: React.FC = () => {
           show: true,
           position: "bottom",
           horizontalAlign: "right",
-          customLegendItems: ["Team A", "Team B", "Team C", "Team D"],
+          customLegendItems: ["Check-in", "App User", "New Added WiFi", "Issued mPoints"],
           inverseOrder: false,
           offsetY: 7,
           labels: {
@@ -195,24 +283,24 @@ const Home: React.FC = () => {
             },
           },
         },
-        title: {
-          text: "Check In Growth Quantity",
-          style: {
-            color: "#fff",
-            fontSize: "20px",
-            fontWeight: 600,
-          },
-        },
-        subtitle: {
-          text: `${calculateSumText()}`,
-          margin: 14,
-          style: {
-            fontSize: "14px",
-            color: "RGB(78,171,240,0.8)",
-            fontFamily: "Inter",
-            fontWeight: "light",
-          },
-        },
+        // title: {
+        //   text: "Check In Growth Quantity",
+        //   style: {
+        //     color: "#fff",
+        //     fontSize: "20px",
+        //     fontWeight: 600,
+        //   },
+        // },
+        // subtitle: {
+        //   text: `${calculateSumText()}`,
+        //   margin: 14,
+        //   style: {
+        //     fontSize: "14px",
+        //     color: "RGB(78,171,240,0.8)",
+        //     fontFamily: "Inter",
+        //     fontWeight: "light",
+        //   },
+        // },
         markers: {
           size: 3,
           strokeWidth: 1,
@@ -243,7 +331,22 @@ const Home: React.FC = () => {
   const ReactApexChart = require("react-apexcharts").default;
 
   return (
-    <div className="bg-[#010127] mx-auto sm:px-14 p-4 sm:py-10">
+    <div className="bg-[#010127] mx-auto px-4 md:px-24 py-2 md:py-10">
+      <p className="text-xl font-bold mx-4">Mix Charts Quatity</p>
+      <div className="block xl:flex xl:justify-between my-4 mx-4">
+        <div className="flex gap-1 md:gap-3">
+          {showSeriesSum()}
+        </div>
+        <div className="flex items-center mt-3 xl:mt-0 gap-2 text-sm tracking-wide">
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">Day</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">3 Day</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">Week</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">3 Week</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">Month</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">3 Month</a>
+            <a href="" className="bg-[#ffffff40] hover:bg-[#ffffff60] rounded-md text-white opacity-70 px-3 py-1">Year</a>
+          </div>
+      </div>
       <ReactApexChart
         options={chartOptions}
         series={chartOptions.series}
